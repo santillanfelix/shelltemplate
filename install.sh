c8b5429feaeb202b3b946afb3c8a32db04e6629e
Andres Aquino <aquino@hp.com>
2011-04-01 12:31:16 -0600

    TASK: cambios menores, nada relevante.

--- install.sh
+++ install.sh
@@ -3,11 +3,49 @@
 
 # install.sh 
 # =
-# 2010, Hewlett-Packard Company
-# Andres Aquino <aquino@hp.com>
-# All rights reserved.
+#
+# Andres Aquino <aquino(at)hp.com>
+# Hewlett-Packard Company
 # 
 
+echo "[1] - Creating structure..."
+mkdir -p ~/bin
+mkdir -p ~/manuals/man1
+
+echo "[2] - Migrating all config files to new version..."
+cd ~/soya.git/setup/
+[ -d ~/soya ] && cp -rp ~/soya/setup/*.conf .
+for ap in *.conf
+do
+  ln -sf ../soya.sh ${ap%.conf}.start
+  ln -sf ../soya.sh ${ap%.conf}.stop
+  ln -sf ../soya.sh ${ap%.conf}.check
+  ln -sf ../soya.sh ${ap%.conf}.logson
+  ln -sf ../soya.sh ${ap%.conf}.logsoff
+  ln -sf ../soya.sh ${ap%.conf}.syslogson
+  ln -sf ../soya.sh ${ap%.conf}.syslogsoff
+  ln -sf ../soya.sh ${ap%.conf}.dblogson
+  ln -sf ../soya.sh ${ap%.conf}.dblogsoff
+done
+
+echo "[3] - Switching to new version..."
+cd ~
+[ -d ~/soya.old ] && rm -fr ~/soya.old
+[ -d ~/soya ] && mv ~/soya ~/soya.old
+[ -d ~/soya.git ] && mv ~/soya.git ~/soya
+
+echo "[4] - Installing unix documentation..."
+cp ~/soya/man1/soya.1 ~/manuals/man1/
+ln -sf ~/soya/soya.sh ~/soya/soya.version
+ln -sf ~/soya/soyarc ~/.soyarc
+ln -sf ~/soya/screenrc ~/.screenrc
+
+echo "[5] - Fixing permissiont..."
+chmod 0640 ~/soya/install.sh 
+chmod 0550 ~/soya/soya.sh
+
+echo "[*] - That's all..."
+#
 # get current directory
 CURRENT=`dirname ${0}`
 if [ ${CURRENT} == "." ]
